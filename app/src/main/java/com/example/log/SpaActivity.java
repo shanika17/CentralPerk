655af7fa0947c0d4aef60b8a5d6488d38b956e02
hiru99 <hirunimjayasinghe@gmail.com>
2021-09-25 00:43:53 +0530

    spa activity

--- app/src/main/java/com/example/log/SpaActivity.java
+++ app/src/main/java/com/example/log/SpaActivity.java
@@ -10,5 +10,6 @@ public class SpaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spa);
+        //hiruni
     }
 }
