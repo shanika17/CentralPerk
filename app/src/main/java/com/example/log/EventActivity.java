8fd3385ebddf3942228bf30ef0cc72b98679c067
NethmiPelendagama <nethmipelendagama@gmail.com>
2021-09-25 05:38:48 +0530

    EventActivityUpdate

--- app/src/main/java/com/example/log/EventActivity.java
+++ app/src/main/java/com/example/log/EventActivity.java
@@ -1,15 +1,147 @@
 package com.example.log;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
 
 public class EventActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_event_button;
+    ImageView empty_imageview;
+    TextView no_data;
+
+    MyDatabaseHelper myDatabaseHelper;
+    ArrayList<String> event_id, name, email, phoneNo, noOfGuests, eventDate, eventType, noOfRooms, requirements;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        add_event_button = findViewById(R.id.add_event_button);
+        empty_imageview = findViewById(R.id.empty_imageview);
+        no_data = findViewById(R.id.no_data);
+        add_event_button.setOnClickListener((v -> {
+            Intent intent = new Intent(EventActivity.this, AddEventActivity.class);
+            startActivity(intent);
+        }));
+
+        myDatabaseHelper = new MyDatabaseHelper(EventActivity.this);
+        event_id = new ArrayList<>();
+        name = new ArrayList<>();
+        email = new ArrayList<>();
+        phoneNo = new ArrayList<>();
+        noOfGuests = new ArrayList<>();
+        eventDate = new ArrayList<>();
+        eventType = new ArrayList<>();
+        noOfRooms = new ArrayList<>();
+        requirements = new ArrayList<>();
+
+        storeDataInArray();
+
+        //this**
+        customAdapter = new CustomAdapter(EventActivity.this, this, event_id, name, email, phoneNo, noOfGuests, eventDate, eventType, noOfRooms, requirements);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(EventActivity.this));
+
     }
-    //Palaya
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1) {
+            recreate();
+        }
+
+    }
+
+    void storeDataInArray() {
+        Cursor cursor = myDatabaseHelper.readAllData();
+        if(cursor.getCount() == 0) {
+            empty_imageview.setVisibility(View.VISIBLE);
+            no_data.setVisibility(View.VISIBLE);
+        }else{
+            while (cursor.moveToNext()){
+                event_id.add(cursor.getString(0));
+                name.add(cursor.getString(1));
+                email.add(cursor.getString(2));
+                phoneNo.add(cursor.getString(3));
+                noOfGuests.add(cursor.getString(4));
+                eventDate.add(cursor.getString(5));
+                eventType.add(cursor.getString(6));
+                noOfRooms.add(cursor.getString(7));
+                requirements.add(cursor.getString(8));
+
+
+            }
+            empty_imageview.setVisibility(View.GONE);
+            no_data.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.delete_all){
+            //Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+            confirmDialog();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete All?");
+        builder.setMessage("Are you sure you want to delete all Data?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(EventActivity.this);
+                myDB.deleteAllData();
+                //Refresh activity
+                Intent intent = new Intent(EventActivity.this, EventActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
+    }
 }
